ad030e55ad0fdac54fb12ae49fc44916aade474e
Jose Daniel Campos Galán <josedaniel.campos@clupik.com>
2025-01-13 18:29:24 +0100

    Fixes an new elements

---
+++ Components/Hitbox/hitbox.gd
@@ -0,0 +1,15 @@
+class_name Hitbox extends Area2D
+
+signal damage_was_take( damage:float)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func take_damage(damage:float) ->void:
+	damage_was_take.emit(damage)
+	pass
---
+++ Components/Hurtbox/hurtbox.gd
@@ -0,0 +1,16 @@
+class_name Hurtbox extends Area2D
+
+@export var damage:float = 10.0;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_entered.connect(on_area_entered)
+	pass # Replace with function body.
+
+func on_area_entered(area:Area2D) -> void:
+	if area is Hitbox :
+		area.take_damage(damage)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
